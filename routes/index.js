9478a25aca3387c72a6363cd444c8afda3b96176
CalvinLee0316 <calee@ucsd.edu>
2020-12-29 13:33:42 +0900

    api working, games array contains all of today's games

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,43 @@ router.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-router.get("/scores", (req, res) => {
-  req.headers({
-	"x-rapidapi-key": "d060dde1cfmsh7a0dd2b1d3d141bp1e5c6bjsn20e2b979a0dc",
-	"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-	"useQueryString": true
-  });
 
-  let response = nodeFetch("https://api-nba-v1.p.rapidapi.com/games/live/");
-  response.then((rep) => {
-    return rep.json();
-  }).then((rep_data) => {
-    console.log(rep_data);
-  });
+router.get("/scores", (req, res) => {
+  nodeFetch("https://api-nba-v1.p.rapidapi.com/games/live/", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "d060dde1cfmsh7a0dd2b1d3d141bp1e5c6bjsn20e2b979a0dc",
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+      }
+    })
+    .then(response => response.json())
+    .then(data=>{
+      let games= []
+      data.api.games.forEach(game=>{
+        games.push({
+          Home: game.hTeam.fullName,
+          Away: game.vTeam.fullName,
+          HomeScore: game.hTeam.score.points,
+          AwayScore: game.vTeam.score.points
+        })
+      })
+      console.log(games)
+    })
+
+
+
+  // req.headers({
+  // "x-rapidapi-key": "d060dde1cfmsh7a0dd2b1d3d141bp1e5c6bjsn20e2b979a0dc",
+  // "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+  // "useQueryString": true
+  // });
+  //
+  // let response = nodeFetch("https://api-nba-v1.p.rapidapi.com/games/live/");
+  // response.then((rep) => {
+  //   return rep.json();
+  // }).then((rep_data) => {
+  //   console.log(rep_data);
+  // });
 });
 
 router.get("*", (req, res) => {
