1b410052636dd16feaef3ee3fce7ac70fb2494ec
CalvinLee0316 <calvinlee0316@gmail.com>
2021-01-05 15:18:15 +0900

    added a simple, basic scoreboard front end, some teams do not have logos, so have to add manually later

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ let indexRoute = require("./routes/index.js");
 // App set up
 let app = express();
 app.use(express.static("public"));
+app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 //Routes
--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,14 @@ router.get("/scores", (req, res) => {
   nodeFetch("https://api-nba-v1.p.rapidapi.com/games/live/", {
       "method": "GET",
       "headers": {
-        "x-rapidapi-key": "d060dde1cfmsh7a0dd2b1d3d141bp1e5c6bjsn20e2b979a0dc",
+        "x-rapidapi-key": "04be869764msh1173a225149d4f4p1b5c0fjsn2f218b338a38",
         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
       }
     })
     .then(response => response.json())
-    .then(data=>{
-      let games= []
-      data.api.games.forEach(game=>{
+    .then(data => {
+      let games = []
+      data.api.games.forEach(game => {
         games.push({
           Home: game.hTeam.fullName,
           HomeLogo: game.hTeam.logo,
@@ -28,10 +28,43 @@ router.get("/scores", (req, res) => {
           AwayScore: game.vTeam.score.points
         })
       })
-      res.render("index", {
-        games: games
+      if (games.length > 0) {
+        res.render("index", {
+          games: games
+        })
+      } else {
+        //If there are no live games, show games from today that ended
+        var today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+        today = yyyy + "-" + mm + "-" + dd;
+        nodeFetch(`https://api-nba-v1.p.rapidapi.com/games/date/${today}`, {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-key": "04be869764msh1173a225149d4f4p1b5c0fjsn2f218b338a38",
+              "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+            }
+          })
+          .then(res => res.json())
+          .then(dat => {
+            dat.api.games.forEach(game => {
+              games.push({
+                Home: game.hTeam.fullName,
+                HomeLogo: game.hTeam.logo,
+                Away: game.vTeam.fullName,
+                AwayLogo: game.vTeam.logo,
+                HomeScore: game.hTeam.score.points,
+                AwayScore: game.vTeam.score.points
+              })
+            })
+            res.render("index", {
+              games: games
+            })
+          })
+
       }
-      console.log(games)
+
     })
 
 
