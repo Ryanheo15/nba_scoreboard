b84d3fa69d7190ee21d0fe3bdb869a15bb3aab8b
CalvinLee0316 <calvinlee0316@gmail.com>
2021-01-06 12:29:01 +0900

    shows game status, shows all games of the day, not just live ones

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/scores", (req, res) => {
+  //add live games
   nodeFetch("https://api-nba-v1.p.rapidapi.com/games/live/", {
       "method": "GET",
       "headers": {
@@ -17,52 +18,75 @@ router.get("/scores", (req, res) => {
     .then(response => response.json())
     .then(data => {
       let games = []
+      let teams = []
       data.api.games.forEach(game => {
+        console.log(game)
+        const Home = game.hTeam.fullName;
+        const Away = game.vTeam.fullName;
         games.push({
-          Home: game.hTeam.fullName,
+          Home: Home,
           HomeLogo: game.hTeam.logo,
-          Away: game.vTeam.fullName,
+          Away: Away,
           AwayLogo: game.vTeam.logo,
           HomeScore: game.hTeam.score.points,
-          AwayScore: game.vTeam.score.points
+          AwayScore: game.vTeam.score.points,
+          status: "Live"
+        })
+        teams.push({
+          Home: Home,
+          Away: Away
         })
       })
-      if (games.length > 0) {
-        res.render("index", {
-          games: games
+      //add non live games
+      var today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      const yyyy = today.getFullYear();
+      today = yyyy + "-" + mm + "-" + dd;
+      nodeFetch(`https://api-nba-v1.p.rapidapi.com/games/date/${today}`, {
+          "method": "GET",
+          "headers": {
+            "x-rapidapi-key": process.env.API_KEY,
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+          }
         })
-      } else {
-        //If there are no live games, show games from today that ended
-        var today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        const yyyy = today.getFullYear();
-        today = yyyy + "-" + mm + "-" + dd;
-        nodeFetch(`https://api-nba-v1.p.rapidapi.com/games/date/${today}`, {
-            "method": "GET",
-            "headers": {
-              "x-rapidapi-key": process.env.API_KEY,
-              "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+        .then(res => res.json())
+        .then(dat => {
+          dat.api.games.forEach(game => {
+            const Home = game.hTeam.fullName;
+            const Away = game.vTeam.fullName;
+            const currTeam = {
+              Home: Home,
+              Away: Away
             }
-          })
-          .then(res => res.json())
-          .then(dat => {
-            dat.api.games.forEach(game => {
+            //check if game is live, already in games
+            var isNotInArray = teams.find(function(el){ return el.Home === Home})== undefined;
+            let status;
+            if(game.statusGame === "Finished"){
+              status = "Final"
+            }else{
+              status = "Scheduled"
+            }
+            if (isNotInArray) {
               games.push({
                 Home: game.hTeam.fullName,
                 HomeLogo: game.hTeam.logo,
                 Away: game.vTeam.fullName,
                 AwayLogo: game.vTeam.logo,
                 HomeScore: game.hTeam.score.points,
-                AwayScore: game.vTeam.score.points
+                AwayScore: game.vTeam.score.points,
+                status: status
               })
-            })
-            res.render("index", {
-              games: games
-            })
-          })
+              teams.push(currTeam)
+            }
 
-      }
+          })
+          res.render("index", {
+            games: games
+          })
+        })
+
+
 
     })
 
