9b6cb6b0daf430dda9b923cda6ccb5f2b2bad6b1
CalvinLee0316 <calvinlee0316@gmail.com>
2021-01-18 18:17:35 +0900

    deploy test

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(indexRoute);
 
 //Listen
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+let port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
